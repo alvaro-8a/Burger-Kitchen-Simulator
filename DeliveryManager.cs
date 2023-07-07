61d1fe7e1897a91055886b4c704a9a59bf158f27
ablano00 <alvaroblanco8a@gmail.com>
2023-07-07 11:40:13 +0200

    Delivery variables refactored

--- DeliveryManager.cs
+++ DeliveryManager.cs
@@ -5,22 +5,35 @@ using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
+	// Event when a recipe is added to the waiting list
 	public event EventHandler OnRecipeSpawned;
+	// Event when a recipe has been completed
 	public event EventHandler OnRecipeCompleted;
+	// Event when recipe has been successfully delivered
 	public event EventHandler OnRecipeSuccess;
+	// Event when the recipe that tried to be delivered wasn't on the waiting list
 	public event EventHandler OnRecipeFailed;
 
+	/** SINGLETON PATTERN **/
 	public static DeliveryManager Instance { get; private set; }
+
+	// List of all Delivery counters on the scene
 	public List<DeliveryCounter> pickupPoints;
 
+	// Scriptable Object with the List of all the recipes
 	[SerializeField] private RecipeListSO recipeListSO;
+	// Customer (AI) GameObject
 	[SerializeField] private GameObject customer;
+	// Position to Spawn Customers ("Entry to the Burger")
 	[SerializeField] private Transform customerSpawnPoint;
 
-
+	// List of waiting recipes
 	private List<RecipeSO> waitingRecipeSOList;
-	private float spawnRecipeTimer;
-	private float spawnRecipeTimerMax = 4f;
+	// Time left to spawn another Customer
+	private float spawnCustomerTimer;
+	// Time between Customers spawn
+	private float spawnCustomerTimerMax = 4f;
+	// Max amount of recipes on waiting list
 	private int waitingRecipeMax = 4;
 	private int successfulRecipesAmount;
 	private List<DeliveryCounter> availablePickupPoints;
@@ -42,10 +55,10 @@ public class DeliveryManager : MonoBehaviour
 
 	private void Update()
 	{
-		spawnRecipeTimer -= Time.deltaTime;
-		if (spawnRecipeTimer <= 0f)
+		spawnCustomerTimer -= Time.deltaTime;
+		if (spawnCustomerTimer <= 0f)
 		{
-			spawnRecipeTimer = spawnRecipeTimerMax;
+			spawnCustomerTimer = spawnCustomerTimerMax;
 
 			if (KitchenGameManager.Instance.IsGamePlaying() && waitingRecipeSOList.Count < waitingRecipeMax && availablePickupPoints.Count > 0)
 			{
